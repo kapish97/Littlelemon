5b5c016889f9fe7d06e01d203c124868e1879a29
kapish97 <kapish.261997@gmail.com>
2023-05-07 18:49:22 +0530

    Booking and Menu tables has beem created

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(max_length=6)
+    BookingDate = models.DateTimeField()
+
+class Menu(models.Model):
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10,decimal_places=2)
+    Inventory = models.IntegerField(max_length=5)
+    
