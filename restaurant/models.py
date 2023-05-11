9b976c5023e434b8d49b1bd820f42d1a0f8fa73f
kapish97 <kapish.261997@gmail.com>
2023-05-11 15:36:20 +0530

    apps test.py file deleted

--- restaurant/models.py
+++ restaurant/models.py
@@ -13,3 +13,5 @@ class Menu(models.Model):
     Price = models.DecimalField(max_digits=10,decimal_places=2)
     Inventory = models.IntegerField()
     
+    def __str__(self):
+        return f'{self.Title}:{str(self.Price)}'
